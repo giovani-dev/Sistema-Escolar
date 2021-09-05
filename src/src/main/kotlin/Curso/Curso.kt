e57a60f7758279b8c34b8e2e42d25f090b8a9d28
Giovani L. Zanini <giovanilzanini@hotmail.com>
2021-09-05 19:28:17 -0300

    Fix: add professor no curso

--- src/src/main/kotlin/Curso/Curso.kt
+++ src/src/main/kotlin/Curso/Curso.kt
@@ -1,16 +1,39 @@
 package Curso
+import Aluno.InfoAluno
 import Base.InfoInterface
 import Base.InfoDataClass
+import Professor.InfoProfessor
 
 
 class Curso: InfoInterface {
-    lateinit override var info: InfoDataClass
+    override var info: InfoDataClass
+    override lateinit var data: InfoDataClass
 
-    constructor(nome: String, codigo: Int) {
-        this.info = InfoCurso(nome, codigo)
+    constructor(
+        nome: String,
+        codigo: Int,
+        professor_titular: InfoProfessor,
+        professor_adjunto: InfoProfessor,
+        qtde_maxima_de_alunos: Int,
+        alunos_matriculados: List<InfoAluno>
+    ) {
+        this.info = InfoCurso(
+            nome = nome,
+            codigo = codigo,
+            professor_titular = professor_titular,
+            professor_adjunto = professor_adjunto,
+            qtde_maxima_de_alunos = qtde_maxima_de_alunos,
+            alunos_matriculados = alunos_matriculados
+        )
     }
 
     override fun equals(other: Any?): Boolean {
         return (other is Curso) && (other.info.codigo == this.info.codigo)
     }
+
+    override fun create(): InfoInterface {
+        this.data = this.info
+        return this
+    }
+
 }
